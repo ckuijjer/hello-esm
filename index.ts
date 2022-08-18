4a7b74ceb10dc16356b7ad45dbb073363dbbcadd
Casper Kuijjer <casper@kuijjer.com>
2022-08-18 16:32:47 +0200

    more stuff

--- index.ts
+++ index.ts
@@ -12,6 +12,9 @@ const levenshteinDistance = leven(HELLO_WORLD, HALLO_WERELD);
 
 console.log(JSON.stringify({ result }, null, 2));
 
+const lastElement = [1, 2, 3, 4, 5].at(-1);
+console.log(lastElement);
+
 debug(
   "levenshteinDistance |%s|%s|%d",
   HELLO_WORLD,
